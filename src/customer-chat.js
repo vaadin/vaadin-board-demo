9f4c66051b64ff7036d5f4b911e84f03fa702bbe
Kari Söderholm <haprog@vaadin.com>
2017-05-16 10:28:18 +0300

    Load Vaadin customer chat (when hosted on demo.vaadin.com)

---
+++ src/customer-chat.js
@@ -0,0 +1,9 @@
+(function(){
+  if (window.location.hostname !== 'demo.vaadin.com') return;
+  
+  window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
+  d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+  _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute("charset","utf-8");
+  $.src="https://v2.zopim.com/?16Ussk779zFj2X2OcAvLsP9FecZpt3qG";z.t=+new Date;$.
+  type="text/javascript";e.parentNode.insertBefore($,e)})(document,"script");
+})();
